e17fa8b7e510440f8ff69f8e39c72ec5517c0bb1
Muskaan Manocha <70715304+muskaan-codes@users.noreply.github.com>
2023-04-20 18:31:34 -0400

    OOPs + Pythonic version + Added functionality to make sure RSA key pair is generated only once for each pair.

---
+++ main/api/proj_4a/README.md
@@ -0,0 +1,28 @@
+Description: This folder comprises of different encryption APIs and Utilities coded in Python 3.x for the Python SDK for AtSign as a part of 682 course (MS CS - UMass Boston) Project 4a
+
+More about the Encryption API: It makes use of the following encryption and decryption algorithms:
+RSA Encryption 2048 (Sign, Verify, Encrypt, Decrypt) & Keypair Generation, Base64 Encode/Decode, AES CTR 256 Encrypt/Decrypt, & Key Generation
+
+Table of Contents:
+1)Structure
+2)Usage
+3)Algorithm
+4)Collaborators
+
+Structure:
+Abstract Class: Encryption.py
+Implementation Classes: Format: xyz_encryption.py
+Utility: encryption_util.py
+
+Usage:
+1) Use the EncryptionUtil in the main SDK to get the functionality of all the algorithms. 
+2) xyz_encryption is for individual implementation of an encryption algorithm, it has to implement the abstract class encryption.py and implement the abstract methods encrypt() and decrypt().
+
+Algorithm:
+
+
+
+Collaborators:
+Muskaan Manocha
+Prem Desai
+Yesheswani Murthy
---
+++ main/api/proj_4a/rsa_encryption.py
@@ -0,0 +1,90 @@
+"""
+-*- coding: utf-8 -*-
+Implementation of the RSA Encryption and Decryption functionality
+Created: April 2023
+Owners: Project 4a: Muskaan Manocha, Prem Desai, Yeshaswini Murthy
+"""
+    
+import rsa
+import base64
+from cryptography.hazmat.primitives import serialization
+from encryption import Encryption
+
+class RsaEncryption(Encryption):
+
+    def generate_key_pair(self, b=2048):
+        """
+        Generate a pair of public and private keys for RSA encryption.
+        
+        Returns:
+            A tuple containing the public key and the private key.
+        """
+        (public_key, private_key) = rsa.newkeys(b)
+        return (public_key, private_key)
+
+    def encrypt(self, message, public_key):
+        """
+        Encrypt a message using RSA encryption with a public key.
+        
+        Args:
+            message (str): The message to be encrypted.
+            public_key (rsa.PublicKey): The public key to be used for encryption.
+        
+        Returns:
+            The encrypted message as a Base64-encoded string.
+        """
+        encoded_message = message.encode()
+        encrypted_message = rsa.encrypt(encoded_message, public_key)
+        return base64.b64encode(encrypted_message).decode()
+
+    def decrypt(self, encrypted_message, private_key):
+        """
+        Decrypt a message using RSA encryption with a private key.
+        
+        Args:
+            encrypted_message (str): The encrypted message as a Base64-encoded string.
+            private_key (rsa.PrivateKey): The private key to be used for decryption.
+        
+        Returns:
+            The decrypted message as a string.
+        """
+        decoded_message = base64.b64decode(encrypted_message)
+        decrypted_message = rsa.decrypt(decoded_message, private_key)
+        return decrypted_message.decode()
+
+    def sign(self, message, private_key, sign='SHA-256'):
+        """
+        Sign a message using RSA encryption with a private key.
+        
+        Args:
+            message (str): The message to be signed.
+            private_key (rsa.PrivateKey): The private key to be used for signing.
+            sign (str): default rsa sign used is SHA-256
+        
+        Returns:
+            The signature as a Base64-encoded string.
+        """
+        encoded_message = message.encode()
+        signature = rsa.sign(encoded_message, private_key, sign)
+        return base64.b64encode(signature).decode()
+
+    def verify(self, message, signature, public_key):
+        """
+        Verify a message's signature using RSA encryption with a public key.
+        
+        Args:
+            message (str): The message whose signature is to be verified.
+            signature (str): The signature to be verified as a Base64-encoded string.
+            public_key (rsa.PublicKey): The public key to be used for verification.
+        
+        Returns:
+            True if the signature is valid, False otherwise.
+        """
+        encoded_message = message.encode()
+        decoded_signature = base64.b64decode(signature)
+        try:
+            rsa.verify(encoded_message, decoded_signature, public_key)
+            return True
+        except:
+            return False
+    
