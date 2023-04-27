4c854f9397c2bb75c763ef6364ef050aa3539dd1
umangshah <shahumang19@gmail.com>
2023-04-27 16:04:25 -0400

    created test cases

--- test-main/atRootConnectionTest.py
+++ test-main/atRootConnectionTest.py
@@ -14,7 +14,29 @@ class AtRootConnectionTest(unittest.TestCase):
         print()
         rootConnection = AtRootConnection.getInstance(verbose=True)
         secondaryAddress = rootConnection.findSecondary("@27barracuda")
-        self.assertTrue(secondaryAddress)
+        self.assertIsNotNone(secondaryAddress)
+
+    def testFindSecondaryFailure(self):
+        print()
+        try:
+            rootConnection = AtRootConnection.getInstance(verbose=True)
+            secondaryAddress = rootConnection.findSecondary("@wrongAtSign")
+        except Exception as e:
+            self.assertEqual("Root lookup returned null for @wrongAtSign", str(e))
+
+    def testFindMultipleSecondaryAddresses(self):
+        print()
+        rootConnection = AtRootConnection.getInstance(verbose=True)
+        secondaryAddress1 = rootConnection.findSecondary("@27barracuda").split(":")
+        secondaryAddress2 = rootConnection.findSecondary("@19total67").split(":")
+        secondaryAddress3 = rootConnection.findSecondary("@wildgreen").split(":")
+        secondaryAddress4 = rootConnection.findSecondary("@colin").split(":")
+        
+        self.assertIsNotNone(secondaryAddress1)
+        self.assertIsNotNone(secondaryAddress2)
+        self.assertIsNotNone(secondaryAddress3)
+        self.assertIsNotNone(secondaryAddress4)
+
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
--- test-main/atSecondaryConnectionTest.py
+++ test-main/atSecondaryConnectionTest.py
@@ -14,6 +14,38 @@ class AtSecondaryConnectionTest(unittest.TestCase):
         secondaryConnection.connect()
         self.assertTrue(secondaryConnection.isConnected())
 
+    def testSecondaryConnectionFailure(self):
+        print()
+        try:
+            rootConnection = AtRootConnection.getInstance(verbose=True)
+            secondaryAddress = rootConnection.findSecondary("@27barracuda")
+            secondaryAddress = secondaryAddress.split(":")
+            secondaryConnection = AtSecondaryConnection(secondaryAddress[0]+"0", secondaryAddress[1], verbose=True)
+            secondaryConnection.connect()
+        except Exception as e:
+            self.assertEqual("[Errno 8] nodename nor servname provided, or not known", str(e))
+
+    def testMultipleSecondaryConnections(self):
+        print()
+        rootConnection = AtRootConnection.getInstance(verbose=True)
+        secondaryAddress1 = rootConnection.findSecondary("@27barracuda").split(":")
+        secondaryConnection1 = AtSecondaryConnection(secondaryAddress1[0], secondaryAddress1[1], verbose=True)
+        secondaryConnection1.connect()
+        secondaryAddress2 = rootConnection.findSecondary("@19total67").split(":")
+        secondaryConnection2 = AtSecondaryConnection(secondaryAddress2[0], secondaryAddress2[1], verbose=True)
+        secondaryConnection1.connect()
+        secondaryAddress3 = rootConnection.findSecondary("@wildgreen").split(":")
+        secondaryConnection3 = AtSecondaryConnection(secondaryAddress3[0], secondaryAddress3[1], verbose=True)
+        secondaryConnection3.connect()
+        secondaryAddress4 = rootConnection.findSecondary("@colin").split(":")
+        secondaryConnection4 = AtSecondaryConnection(secondaryAddress4[0], secondaryAddress4[1], verbose=True)
+        secondaryConnection4.connect()
+        
+        self.assertIsNotNone(secondaryConnection1)
+        self.assertIsNotNone(secondaryConnection2)
+        self.assertIsNotNone(secondaryConnection3)
+        self.assertIsNotNone(secondaryConnection4)
+
 if __name__ == '__main__':
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromTestCase(AtSecondaryConnectionTest)
