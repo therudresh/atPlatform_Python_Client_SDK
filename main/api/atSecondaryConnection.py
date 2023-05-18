d80345a62a30ae848225eb2b99d08bc380c433b5
Varun Shembekar <varun247shembekar@gmail.com>
2023-05-18 12:02:04 -0400

    Update atSecondaryConnection.py

--- main/api/atSecondaryConnection.py
+++ main/api/atSecondaryConnection.py
@@ -1,18 +1,22 @@
 import ssl
 from .atConnection import AtConnection
 
-# The AtSecondaryConnection class is a subclass of AtConnection and represents a connection to a secondary server in the @ protocol. It inherits methods from the parent class to establish a connection, send commands, and parse responses. It also inherits executeCommand(), isConnected(), disconnect(), write(), read(), and str() methods.
+
 class AtSecondaryConnection(AtConnection):
+    """Subclass of AtConnection representing a connection to a secondary server in the @ protocol."""
 
     def __init__(self, host, port, context=ssl.create_default_context(), verbose=False):
+        """Initialize the AtSecondaryConnection object."""
         super().__init__(host, port, context, verbose)
 
     def connect(self):
+        """Establish a connection to the secondary server."""
         super().connect()
         if self.verbose:
             print(f"Secondary Connection Successful")
 
     def parseRawResponse(self, rawResponse):
+        """Parse the raw response from the secondary server."""
         if rawResponse.endswith("@"):
             rawResponse = rawResponse[:-1]
         rawResponse = rawResponse.strip()
